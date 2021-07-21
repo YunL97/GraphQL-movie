81627c332f2548ab114e7251a384eac477b83e52
dbstlr5248!! <leeys9932@naver.com>
2021-07-21 23:09:05 +0900

    fin

--- graphql/db.js
+++ graphql/db.js
@@ -1,77 +1,99 @@
-let movies = [
-  {
-    id: 0,
-    name: "star",
-    score: 0,
-  },
-  {
-    id: 1,
-    name: "star",
-    score: 0,
-  },
-  {
-    id: 2,
-    name: "star",
-    score: 0,
-  },
-  {
-    id: 3,
-    name: "star",
-    score: 0,
-  },
-];
+import fetch from "node-fetch";
 
-export const getMovies = () => movies;
+const API_URL = "https://yts.mx/api/v2/list_movies.json?";
 
-export const getById = (id) => {
-  const filteredMovies = movies.filter((movie) => movie.id === String(id));
-  return filteredMovies[0];
-};
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((movie) => movie.id !== String(id));
-  if (movies.length > cleanedMovies.length) {
-    movie = cleanedMovies;
-    return true;
-  } else {
-    return false;
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  if (limit > 0.6) {
+    REQUEST_URL += `limit=${limit}&`;
   }
-};
 
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
+  if (rating > 0) {
+    REQUEST_URL += `minimum_rating=${rating}`;
+  }
+  return fetch(REQUEST_URL) //node fetch 랑 axios랑 같음 근데 후자가 더편함
+    .then((res) => res.json())
+    .then((json) => {
+      return json.data.movies;
+    });
+  // return fetch(`${API_URL}`)
+  //   .then((res) => res.json())
+  //   .then((json) => json.data.movies);
 };
-// export const people = [
+// let movies = [
 //   {
-//     id: "0",
-//     name: "lee",
-//     age: 25,
-//     gender: "male",
+//     id: 0,
+//     name: "star",
+//     score: 0,
 //   },
 //   {
-//     id: "1",
-//     name: "lee1",
-//     age: 25,
-//     gender: "male",
+//     id: 1,
+//     name: "star",
+//     score: 0,
 //   },
 //   {
-//     id: "2",
-//     name: "lee2",
-//     age: 25,
-//     gender: "male",
+//     id: 2,
+//     name: "star",
+//     score: 0,
+//   },
+//   {
+//     id: 3,
+//     name: "star",
+//     score: 0,
 //   },
 // ];
 
+// export const getMovies = () => movies;
+
 // export const getById = (id) => {
-//   const filteredPeople = people.filter((person) => person.id === String(id));
-//   //   console.log(people.filter((person) => person));
-//   console.log(id);
-//   return filteredPeople[0];
+//   const filteredMovies = movies.filter((movie) => movie.id === id);
+//   return filteredMovies[0];
 // };
-// //export default db;
+
+// export const deleteMovie = (id) => {
+//   const cleanedMovies = movies.filter((movie) => movie.id != id);
+//   if (movies.length > cleanedMovies.length) {
+//     movies = cleanedMovies;
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// export const addMovie = (name, score) => {
+//   const newMovie = {
+//     id: `${movies.length + 1}`,
+//     name,
+//     score,
+//   };
+//   movies.push(newMovie);
+//   return newMovie;
+// };
+// // export const people = [
+// //   {
+// //     id: "0",
+// //     name: "lee",
+// //     age: 25,
+// //     gender: "male",
+// //   },
+// //   {
+// //     id: "1",
+// //     name: "lee1",
+// //     age: 25,
+// //     gender: "male",
+// //   },
+// //   {
+// //     id: "2",
+// //     name: "lee2",
+// //     age: 25,
+// //     gender: "male",
+// //   },
+// // ];
+
+// // export const getById = (id) => {
+// //   const filteredPeople = people.filter((person) => person.id === String(id));
+// //   //   console.log(people.filter((person) => person));
+// //   console.log(id);
+// //   return filteredPeople[0];
+// // };
+// // //export default db;
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,18 +1,28 @@
-import { getMovies, getById, addMovie } from "./db";
+import { getMovies } from "./db";
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    movie: (_, { id }) => getById(id), //첫번째 인자 _ 는 루트 쿼리
-    // person: (_, args) => {
-    //   console.log(args); 아규먼트 보여줌
-    // },
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
+    // movies: () => getMovies(),
+    movies: (_, { rating, limit }) => getMovies(limit, rating),
   },
 };
 export default resolvers;
 
+// import { getMovies, getById, addMovie,deleteMovie  } from "./db";
+// const resolvers = {
+//   Query: {
+//     movies: () => getMovies(),
+//     movie: (_, { id }) => getById(id), //첫번째 인자 _ 는 루트 쿼리
+//     // person: (_, args) => {
+//     //   console.log(args); 아규먼트 보여줌
+//     // },
+//   },
+//   Mutation: {
+//     addMovie: (_, { name, score }) => addMovie(name, score),
+//     deleteMovie: (_, { id }) => deleteMovie(id),
+//   },
+// };
+// export default resolvers;
+//================================================================
 // import { people, getById } from "./db";
 // const resolvers = {
 //   Query: {
